d0d36a1c5805fcdc5569335156a2d4b89314026e
aritradhabal <aritradhabal36@gmail.com>
2025-10-14 09:48:08 +0530

    refactor: remove unused SVG files and add new CSS styles
    - Deleted several unused SVG files: file.svg, globe.svg, next.svg, vercel.svg, and window.svg.
    - Added a new image file: HashX.png.
    - Introduced a new CSS file (index.css) with custom styles and variables for theming from tweakCN.
    - Updated layout.tsx to include the new CSS and a new font (Space_Grotesk).
    - Modified page.tsx to redirect to the new home page.
    - Created a new home page component in (home)/home/page.tsx.

---
+++ src/app/(home)/home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="h-full w-full flex flex-col gap-y-4 items-center justify-center">
+      HomePage
+    </div>
+  );
+};
+
+export default Home;
